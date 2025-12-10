74ee17576ec7f2e64d45ce34b04284c2babf6adb
Franklin Azriel <FranklinAzriel@FranklinAzriel.MY.ID>
2025-12-10 23:34:28 +0700

    feat(oracle-cloud): add new machine configuration for Oracle Cloud instance

--- clan.nix
+++ clan.nix
@@ -6,49 +6,55 @@
 		description = "Personal NixOS machines for Atlantis";
 	};
 
-  	inventory.machines = {
-		# Define machines here.
-		# jon = { };
-  	};
+  	inventory = {
+		machines = {
+			# Define machines here.
+			# Tags schema:
+			# [Form] [Location] [Owner] [Country] etc
+			oracle-cloud = {	
+				tags = [ "virtual" "cloud" "personal" "singapore" ];
+			};
+  		};
 
-  	# Docs: See https://docs.clan.lol/services/definition/
-  	inventory.instances = {
+  		# Docs: See https://docs.clan.lol/services/definition/
+  		instances = {
+			# Docs: https://docs.clan.lol/services/official/admin/
+			# Admin service for managing machines
+			# This service adds a root password and SSH access.
+			admin = {
+	  			roles.default.tags.all = { };
+	  			roles.default.settings.allowedKeys = {
+					# Insert the public key that you want to use for SSH access.
+					# All keys will have ssh access to all machines ("tags.all" means 'all machines').
+					# Alternatively set 'users.users.root.openssh.authorizedKeys.keys' in each machine
+					"Trusted Key" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILl50pmugL84aI4kD038ls1NKgyDAv5oERwESf3SAYZT";
+					"Trusted Backup Key" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMuViZcCcDLwNC1rJ26l8eBUV6DitM/O8B+akDN/Q9y8";
+	  			};
+			};
 
-		# Docs: https://docs.clan.lol/services/official/admin/
-		# Admin service for managing machines
-		# This service adds a root password and SSH access.
-		admin = {
-	  		roles.default.tags.all = { };
-	  		roles.default.settings.allowedKeys = {
-				# Insert the public key that you want to use for SSH access.
-				# All keys will have ssh access to all machines ("tags.all" means 'all machines').
-				# Alternatively set 'users.users.root.openssh.authorizedKeys.keys' in each machine
-				"admin-machine-1" = "__YOUR_PUBLIC_KEY__";
-	  		};
-		};
+			# Docs: https://docs.clan.lol/services/official/zerotier/
+			# The lines below will define a zerotier network and add all machines as 'peer' to it.
+			# !!! Manual steps required:
+			#   - Define a controller machine for the zerotier network.
+			#   - Deploy the controller machine first to initialize the network.
+			zerotier = {
+	  			# Replace with the name (string) of your machine that you will use as zerotier-controller
+	  			# See: https://docs.zerotier.com/controller/
+	  			# Deploy this machine first to create the network secrets
+	  			roles.controller.machines."__YOUR_CONTROLLER__" = { };
+	  			# Peers of the network
+	  			# tags.all means 'all machines' will joined
+	  			roles.peer.tags.all = { };
+			};
 
-		# Docs: https://docs.clan.lol/services/official/zerotier/
-		# The lines below will define a zerotier network and add all machines as 'peer' to it.
-		# !!! Manual steps required:
-		#   - Define a controller machine for the zerotier network.
-		#   - Deploy the controller machine first to initialize the network.
-		zerotier = {
-	  		# Replace with the name (string) of your machine that you will use as zerotier-controller
-	  		# See: https://docs.zerotier.com/controller/
-	  		# Deploy this machine first to create the network secrets
-	  		roles.controller.machines."__YOUR_CONTROLLER__" = { };
-	  		# Peers of the network
-	  		# tags.all means 'all machines' will joined
-	  		roles.peer.tags.all = { };
-		};
-
-		# Docs: https://docs.clan.lol/services/official/tor/
-		# Tor network provides secure, anonymous connections to your machines
-		# All machines will be accessible via Tor as a fallback connection method
-		tor = {
-	  		roles.server.tags.nixos = { };
-		};
-  	};
+			# Docs: https://docs.clan.lol/services/official/tor/
+			# Tor network provides secure, anonymous connections to your machines
+			# All machines will be accessible via Tor as a fallback connection method
+			tor = {
+	  			roles.server.tags.nixos = { };
+			};
+  		};
+	};
 
   	# Additional NixOS configuration can be added here.
   	# machines/jon/configuration.nix will be automatically imported.
