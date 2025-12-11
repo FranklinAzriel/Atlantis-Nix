1adf60e9706a86e3bc02dcb5322ef977b6265cdc
Franklin Azriel <FranklinAzriel@FranklinAzriel.MY.ID>
2025-12-11 23:50:53 +0700

    refactor(disko): Change disko templates name from name to gpt-luks-btrfs-subvolumes

--- clan.nix
+++ clan.nix
@@ -1,12 +1,12 @@
 {
-  	# Set metadata for clan
-  	meta = {
+	# Set metadata for clan
+	meta = {
 		name = "Atlantis-Nix";
-  		domain = "ocean";
+		domain = "ocean";
 		description = "Personal NixOS machines for Atlantis";
-	};
+	};	
 
-  	inventory = {
+	inventory = {
 		machines = {
 			# Define machines here.
 			# Tags schema:
@@ -14,27 +14,27 @@
 			oracle-cloud = {	
 				tags = [ "virtual" "cloud" "personal" "singapore" ];
 			};
-            dartnode = {	
+			dartnode = {	
 				tags = [ "virtual" "cloud" "personal" "united-states" ];
 			};
-  		};
+		};
 
-  		# Docs: See https://docs.clan.lol/services/definition/
-  		instances = {
-			# Docs: https://docs.clan.lol/services/official/admin/
-			# Admin service for managing machines
-			# This service adds a root password and SSH access.
+		# Docs: See https://docs.clan.lol/services/definition/
+		instances = {
+		# Docs: https://docs.clan.lol/services/official/admin/
+		# Admin service for managing machines
+		# This service adds a root password and SSH access.
 			admin = {
-	  			roles.default = {
+				roles.default = {
 					tags = [ "personal" ];
-	  				settings.allowedKeys = {
+					settings.allowedKeys = {
 						# Insert the public key that you want to use for SSH access.
 						# All keys will have ssh access to all machines ("tags.all" means 'all machines').
 						# Alternatively set 'users.users.root.openssh.authorizedKeys.keys' in each machine
 						"Trusted Key" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILl50pmugL84aI4kD038ls1NKgyDAv5oERwESf3SAYZT";
 						"Trusted Backup Key" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMuViZcCcDLwNC1rJ26l8eBUV6DitM/O8B+akDN/Q9y8";
 					};
-	  			};
+				};
 			};
 
 			FranklinAzriel-user = {
@@ -56,33 +56,32 @@
 			#   - Define a controller machine for the zerotier network.
 			#   - Deploy the controller machine first to initialize the network.
 			zerotier = {
-	  			# Replace with the name (string) of your machine that you will use as zerotier-controller
-	  			# See: https://docs.zerotier.com/controller/
-	  			# Deploy this machine first to create the network secrets
-	  			roles.controller.machines."oracle-cloud" = { };
-	  			# Peers of the network
-	  			# tags.all means 'all machines' will joined
-	  			roles.peer.tags = [ "personal" ];
+					# Replace with the name (string) of your machine that you will use as zerotier-controller
+					# See: https://docs.zerotier.com/controller/
+					# Deploy this machine first to create the network secrets
+					roles.controller.machines."oracle-cloud" = { };
+					# Peers of the network
+					# tags.all means 'all machines' will joined
+					roles.peer.tags = [ "personal" ];
 			};
-  		};
+		};
 	};
 
-  	# Additional NixOS configuration can be added here.
-  	# machines/jon/configuration.nix will be automatically imported.
-  	# See: https://docs.clan.lol/guides/inventory/autoincludes/
-  	machines = {
+	# Additional NixOS configuration can be added here.
+	# machines/jon/configuration.nix will be automatically imported.
+	# See: https://docs.clan.lol/guides/inventory/autoincludes/
+	machines = {
 		# jon = { config, ... }: {
 		#   environment.systemPackages = [ pkgs.asciinema ];
 		# };
-  	};
+	};
 
-    templates = {
-        disko = { 
-            name = {
-                description = "My custom partitioning scheme with GPT, LUKS and BTRFS subvolumes";
-                path = templates/disk/gpt-luks-btrfs-subvolumes;
-            };
-        };
-    };
-
+	templates = {
+		disko = { 
+			gpt-luks-btrfs-subvolumes = {
+				description = "My custom partitioning scheme with GPT, LUKS and BTRFS subvolumes";
+				path = ./templates/disk/gpt-luks-btrfs-subvolumes;
+			};
+		};
+	};
 }
