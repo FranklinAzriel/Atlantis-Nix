a0cc947a2bcd58316141d00627e3ece45a6232a5
Franklin Azriel <FranklinAzriel@FranklinAzriel.MY.ID>
2025-12-21 23:16:30 +0700

    feat(home-manager): Change git email to FranklinAzriel@FranklinAzriel.MY.ID Idk why

--- home-manager/git.nix
+++ home-manager/git.nix
@@ -3,8 +3,8 @@
         enable = true;
         lfs.enable = true;
         settings = {
-            user.name = "FranklinAzriel";
-            user.email = "Owner@FranklinAzriel.MY.ID";
+            user.name = "Franklin Azriel";
+            user.email = "FranklinAzriel@FranklinAzriel.MY.ID";
         };
         signing.signByDefault = true;
         signing.key = "0x8D02FDA2CB47181C";
