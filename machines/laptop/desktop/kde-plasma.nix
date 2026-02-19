d45f7103e3a793925f6e77c89d9a199f8faad46b
Franklin Azriel <FranklinAzriel@FranklinAzriel.MY.ID>
2026-02-19 10:07:39 +0700

    feat(kde-plasma): Update package exclusions and add new packages
    Enhanced the KDE Plasma configuration by updating the list of excluded
    packages and adding additional essential packages for improved user
    experience and functionality. This includes multimedia tools and
    keyboard support, ensuring a more comprehensive desktop environment.

--- machines/laptop/desktop/kde-plasma.nix
+++ machines/laptop/desktop/kde-plasma.nix
@@ -6,8 +6,21 @@
 	};
 
 	services.desktopManager.plasma6.enable = true;
+
+	environment.plasma6.excludePackages = with pkgs; [
+		kdePackages.elisa # Simple music player aiming to provide a nice experience for its users
+    	kdePackages.kdepim-runtime # Akonadi agents and resources
+    	kdePackages.kmahjongg # KMahjongg is a tile matching game for one or two players
+    	kdePackages.kmines # KMines is the classic Minesweeper game
+    	kdePackages.konversation # User-friendly and fully-featured IRC client
+    	kdePackages.kpat # KPatience offers a selection of solitaire card games
+    	kdePackages.ksudoku # KSudoku is a logic-based symbol placement puzzle
+    	kdePackages.ktorrent # Powerful BitTorrent client
+    	mpv
+	];
 	
 	environment.systemPackages = with pkgs; [
+		#inputs.matugen.packages.${system}.default
         # KDE Plasma basic packages
         kdePackages.plasma-desktop
         kdePackages.plasma-vault
@@ -19,5 +32,14 @@
         # KDE Plasma partition tools
         kdePackages.partitionmanager
         kdiskmark
+
+        kdePackages.krfb
+        kdePackages.plasma-keyboard
+        kdePackages.qtvirtualkeyboard
+		kdePackages.kdenlive
     ];
+
+	environment.variables = {
+		KWIN_IM_SHOW_ALWAYS = "1";
+	};
 }
