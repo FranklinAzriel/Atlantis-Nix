515105d955709d3a6776c4786ced8b799263a3e3
Franklin Azriel <FranklinAzriel@FranklinAzriel.MY.ID>
2026-02-19 10:04:03 +0700

    feat(zram): Use entire RAM for zram

--- modules/zram-generator.nix
+++ modules/zram-generator.nix
@@ -3,10 +3,16 @@
 	services.zram-generator = {
     	enable = true; # Enable zram-generator service
     	settings.zram0 = {
-        	compression-algorithm = "zstd lz4 (type=huge)";
-        	zram-size = "ram * 3 / 4"; # Use 75% of RAM for zram
-        	swap-priority = 100; # Set high priority for zram before other swap devices.
+        	compression-algorithm = "zstd";
+        	zram-size = "ram";
+        	swap-priority = 32767; # Set high priority for zram before other swap devices.
         	fs-type = "swap";
     	};
 	};
+
+	#boot.kernel.sysctl = {
+		#"vm.page-cluster" = 0;
+		#"vm.vfs_cache_pressure" = 500;
+		#"vm.swappiness" = 120;
+	#};
 }
